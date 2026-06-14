684e6e8d6d725c2a2072d0582a0c28c366f50b75
Lautaro Nola <lautaronola@gmail.com>
2026-06-14 09:43:43 -0300

    register authService and constants

--- services/authService.js
+++ services/authService.js
@@ -1,20 +1,42 @@
-import { AUTH_KEY, METHODS, LOGIN_URL } from "../constants/index";
+import {
+  AUTH_KEY,
+  METHODS,
+  LOGIN_URL,
+  REGISTER_URL,
+  LOGIN_ERROR,
+  REGISTER_ERROR,
+} from "../constants/index";
 
 const BASE_URL = process.env.EXPO_PUBLIC_API_URL;
 
 const login = async (email, password) => {
   const response = await fetch(`${BASE_URL}${LOGIN_URL}`, {
     method: METHODS.POST,
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password }),
   });
   const data = await response.json();
+
   if (!response.ok) {
     throw new Error(data.message || LOGIN_ERROR);
   }
 
-  await storageService.store(AUTH_KEY, data);
+  return data;
+};
+
+const register = async (name, email, password) => {
+  const response = await fetch(`${BASE_URL}${REGISTER_URL}`, {
+    method: METHODS.POST,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name, email, password }),
+  });
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || REGISTER_ERROR);
+  }
 
   return data;
 };
 
-export default { login };
+export default { login, register };
