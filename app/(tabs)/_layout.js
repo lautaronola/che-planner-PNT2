2c4c707a8010f5f243048f3939b3026970d2e067
Lautaro Nola <lautaronola@gmail.com>
2026-06-22 19:15:15 -0300

    fix home

--- app/(tabs)/_layout.js
+++ app/(tabs)/_layout.js
@@ -1,5 +1,7 @@
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
-  return <Tabs />;
+  return (
+    <Tabs screenOptions={{ headerShown: false, tabBarStyle: { display: "none" } }} />
+  );
 }
